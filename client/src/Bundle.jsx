560186f8c139063a61b29ff8e38988b557bdebc6
Hypermario <molvautj@gmail.com>
2025-09-02 00:01:33 +0900

    fix merge

--- client/src/Bundle.jsx
+++ client/src/Bundle.jsx
@@ -1,49 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css";
 import Game from "/src/components/Game";
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
-import { useUserData } from './hooks/UseUserData';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useParams,
+} from "react-router-dom";
+import { useUserData } from "./hooks/UseUserData";
 
-
-createRoot(document.getElementById('root')).render(
-  <App />
-)
-
-
-
+createRoot(document.getElementById("root")).render(<App />);
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/:room/:name" element={
-            <><GameIfConnected /></>
-          } />
-          <Route path="*" element={<div>NOT FOUND</div>} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route
+                        path="/:room/:name"
+                        element={
+                            <>
+                                <GameIfConnected />
+                            </>
+                        }
+                    />
+                    <Route path="*" element={<div>NOT FOUND</div>} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 const GameIfConnected = () => {
-  const { room, name } = useParams();
-  // send to other 
-  const isConnected = useUserData({ room, name });
-  if (isConnected) {
-    return (
-      <Game rows={20} columns={10}></Game>
-    );
-  }
-  else {
-    return (
-      <div>Waiting server Connection...</div>
-    );
-  }
+    const { room, name } = useParams();
+    // send to other
+    const { socket, isConnected, error } = useUserData({ room, name });
+    if (isConnected && !error) {
+        return <Game room={room} socket={socket}></Game>;
+    } else {
+        return <div>Waiting server Connection...</div>;
+    }
+};
 
-}
-
-
-export default App
+export default App;
