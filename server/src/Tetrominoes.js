f7135c2092338652b98cea73ca1ea69d5e982174
Hypermario <molvautj@gmail.com>
2025-09-03 23:36:13 +0900

    commented Tetrominoes.js

--- server/src/Tetrominoes.js
+++ server/src/Tetrominoes.js
@@ -1,9 +1,20 @@
+/**
+ * Tetromino list for a game, generate tetrominoes as needed.
+ */
 export default class Tetrominoes {
     constructor() {
         this.tetrominoKeys = [];
         this.generateTetrominoesUntil(6);
     }
 
+    /**
+     * Generate a "bag" of 7 tetrominoes like the real game.
+     *
+     * A tetromino is represented by one of theses letters:
+     * "I", "O", "T", "S", "Z", "J", "L"
+     *
+     * @returns Bag of tetrominoes (list of letter)
+     */
     generateNewBag() {
         const keys = ["I", "O", "T", "S", "Z", "J", "L"];
         var count = keys.length,
@@ -18,16 +29,26 @@ export default class Tetrominoes {
         return keys;
     }
 
-    generateTetrominoesUntil(n) {
-        while (n >= this.tetrominoKeys.length) {
+    /**
+     * Generate bags of 7 tetrominoes until the index is reachable.
+     *
+     * @param {number} index The index needed, will generate until reachable.
+     */
+    generateTetrominoesUntil(index) {
+        while (index >= this.tetrominoKeys.length) {
             const newBag = this.generateNewBag();
             this.tetrominoKeys.push(...newBag);
         }
     }
 
-    // Get tetromino at index i (and generate if needed)
-    getNextTetromino(i) {
-        this.generateTetrominoesUntil(i);
-        return this.tetrominoKeys[i];
+    /**
+     * Get tetromino at index i and generate until the index needed.
+     *
+     * @param {number} i Index of the next tetromino.
+     * @returns Next tetromino as a letter: "I", "O", "T", "S", "Z", "J", "L"
+     */
+    getNextTetromino(index) {
+        this.generateTetrominoesUntil(index);
+        return this.tetrominoKeys[index];
     }
 }
